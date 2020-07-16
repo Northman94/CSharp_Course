bf1a4dd999b750cd113bad6dadd0d33dfb8a59e2
Northman94 <Zhandarm2000@gmail.com>
2020-07-17 01:30:12 +0200

    Variables

---
+++ CSharp_Course/S2E12_Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S2E12_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
