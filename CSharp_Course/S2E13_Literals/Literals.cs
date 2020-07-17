b797e1d3851ecad8c4b1a1a9ebe5e9a5a8d2284a
Northman94 <zhandarm2000@gmail.com>
2020-07-17 14:23:31 +0300

    Binary variation

--- CSharp_Course/S2E13_Literals/Literals.cs
+++ CSharp_Course/S2E13_Literals/Literals.cs
@@ -12,6 +12,21 @@ namespace S2E13_Literals
         static void Variables()
         {
             Console.WriteLine("Hello World!");
+
+
+            int x = 0b11;           // (11 = 3)
+            int y = 0b1001;       // ( = )
+
+            int k = 0b10001001;
+            For more convenience we can write it in this way too:
+            int m = 0b1000_1001;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(k);
+            Console.WriteLine(m);
+
+
             Console.ReadLine();
         }
     }
