be1b3a00974c734503560c3dcf7fd9d0734ba1e2
Northman94 <Zhandarm2000@gmail.com>
2020-07-17 14:35:47 +0200

    Hexadecimal

--- CSharp_Course/S2E13_Literals/Literals.cs
+++ CSharp_Course/S2E13_Literals/Literals.cs
@@ -13,19 +13,31 @@ namespace S2E13_Literals
         {
             Console.WriteLine("Hello World!");
 
-
-            int x = 0b11;           // (11 = 3)
-            int y = 0b1001;       // ( = )
+            // Binary:
+            int x = 0b11;           //(11 = 3)
+            int y = 0b1001;        //( 1001 = 9)
 
             int k = 0b10001001;
-            //For more convenience we can write it in this way too:
-            int m = 0b1000_1001;
+            //For more convenience we can write it in this way too with "_" in any place:
+            int m = 0b10_00_1001;
 
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(k);
             Console.WriteLine(m);
 
+            Console.WriteLine("\n");
+
+            // Hexadecimal:
+            x = 0x1F;
+            y = 0xFF0D;
+            k = 0x1FAB30EF;
+            m = 0x1FAB_30EF;  // Also can use "_" in any place.
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(k);
+            Console.WriteLine(m);
 
             Console.ReadLine();
         }
