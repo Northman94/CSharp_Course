556bb8827046296cf733706a1fa002cf96bab39e
Northman94 <Zhandarm2000@gmail.com>
2020-07-21 23:09:11 +0200

    checked { }

---
+++ CSharp_Course/S2E15_Overflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace S2E15_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            checked // !!!
+            {
+                uint x = uint.MaxValue;
+
+                Console.WriteLine(x); // 4294967295
+
+                x += 1;
+
+                Console.WriteLine(x); // 0 = Overflow
+
+                x -= 1;
+
+                Console.WriteLine(x); // 4294967295
+            }
+
+        }
+    }
+}
