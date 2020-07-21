2056abdb25dc861608b9b88942791237f9f92de4
Northman94 <Zhandarm2000@gmail.com>
2020-07-21 20:17:02 +0200

    visibility

---
+++ CSharp_Course/S2E14_Public_Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace S2E14_Public_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            {
+                var b = 2;
+                {
+                    var c = 3;
+
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                }
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                //Console.WriteLine(c);
+            }
+            Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+        }
+    }
+}
