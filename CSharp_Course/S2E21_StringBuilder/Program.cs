c22d289b3441243ee0ccf8103c8639b567e6c11f
Northman94 <Zhandarm2000@gmail.com>
2020-07-29 01:34:25 +0200

    s2e21-22_String builder & esc-sequiences_Part 1

---
+++ CSharp_Course/S2E21_StringBuilder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace S2E21_StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // StringBuilder is the Fastest solution for big strings Concatination
+            StringBuilder sb = new StringBuilder();
+            // Requires:  using System.Text;
+            sb.Append("Great ");
+            sb.Append("day");
+            sb.AppendLine("!");  // Transfers cursor to the new line
+           
+            string concatResult = sb.ToString();
+            Console.WriteLine(concatResult);
+        }
+    }
+}
---
+++ CSharp_Course/S2E22_String_Formatting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace S2E22_String_Formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age = 30;
+
+            string str1 = "My name is" + name + "and I'm" + age + "y.o."; // Same
+            
+            string str2 = string.Format("My name is {0} and I'm {1} y.o.", name, age);
+
+            //String interpolation: $"Text & {variable}";
+            string str3 = $"My name is {name} and I'm {age} y.o.";
+            
+            Console.WriteLine(str2);
+
+            // Escape-sequience
+            // \n = new line
+            string str4 = "The glass is \n half-full";
+            //Or
+            str4 = $"The glass is {Environment.NewLine} half-empty";
+            // $"{Environment.NewLine}"  is cros-platform.
+            // \n  = works differently on various platforms
+
+
+            // \t = tabulation
+            string str5 = "I'm\t30";  // I'm    30
+            
+
+            string str6 = "Phillip is a \"GOOD\" programmer";
+
+            //Slash for slash // Экранирование
+            string str7 = "C:\\tmp\\best_file.txt";
+            Console.WriteLine(str7);
+
+            // Verbatim strings for same purpose
+            string str8 = @"C:\tmp\best_file.txt";
+
+            Console.WriteLine(str8);
+            
+
+        }
+    }
+}
