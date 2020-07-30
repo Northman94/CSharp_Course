e1dd636d1c6eaf77639e66342571712929667663
Northman94 <Zhandarm2000@gmail.com>
2020-07-31 00:05:51 +0200

    S2E23_Console

---
+++ CSharp_Course/S2E23_Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace S2E23_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name ?\n");
+
+            string name = Console.ReadLine();
+            string line1 = $"Your age is {name}";
+            Console.WriteLine(line1);
+
+
+            Console.WriteLine("\nWhat is your age ?\n");
+            string input = Console.ReadLine();
+            int age = int.Parse(input); //string to int
+
+            string sentence = $"Your age is {age}";
+            Console.WriteLine(sentence);
+
+            Console.Clear(); // Clears console
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.Write("\n"); // = Console.WriteLine("");
+        }
+    }
+}
