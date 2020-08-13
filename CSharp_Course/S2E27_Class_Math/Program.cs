bb935c38d983b9f2c8e5d509f3cc047e16b0f882
Northman94 <Zhandarm2000@gmail.com>
2020-08-13 23:32:11 +0200

    S2E27_Class_Math

---
+++ CSharp_Course/S2E27_Class_Math/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace S2E27_Class_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 8;
+
+            long c = Math.BigMul(a, b);
+            // Multiplies 2x ints & stores to long
+            Console.WriteLine(Math.BigMul(a, b)); // 40
+
+            Console.WriteLine(Math.Pow(a,b)); // a^b // (a) в степени (b)
+
+            Console.WriteLine(Math.Sqrt(9)); // 3
+
+            Console.WriteLine(Math.Round(1.7));    // 2
+            Console.WriteLine(Math.Round(1.3));   // 1
+            Console.WriteLine(Math.Round(1.5));  // 2
+            Console.WriteLine(Math.Round(2.5)); // 2 same as .ToEven
+            Console.WriteLine();
+
+            Console.WriteLine(Math.Round(2.5, MidpointRounding.AwayFromZero)); // 3
+            // Rounding to the nearest numb away from zero
+
+            Console.WriteLine(Math.Round(2.5, MidpointRounding.ToEven)); // 2 same as Round
+            // Rounding to nearest even (чётное) numb. Used in Banking
+        }
+    }
+}
+ 
