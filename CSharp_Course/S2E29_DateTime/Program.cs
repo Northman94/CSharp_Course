b66a7c9d27c2b3c6055be0ee6dba143ba6fc5a7e
Northman94 <Zhandarm2000@gmail.com>
2020-08-15 14:47:08 +0200

    S2E29_DateTime

---
+++ CSharp_Course/S2E29_DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace S2E29_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now1 = DateTime.Now;
+            Console.WriteLine(now1.ToString());
+
+            Console.WriteLine($"It's {now1.Date}, {now1.Hour}:{now1.Minute}");
+
+            DateTime t2 = new DateTime(2016, 8, 15);
+            //I no time added => midnight 00:00
+
+            DateTime dayAdition = t2.AddDays(1);
+            Console.WriteLine(dayAdition);
+
+            TimeSpan ts = now1 - t2; //Same v
+            ts = now1.Subtract(t2); // Same ^
+
+            Console.WriteLine(ts.Days);
+
+            // Days vs TotalDays returns double (part of the day)
+        }
+    }
+}
