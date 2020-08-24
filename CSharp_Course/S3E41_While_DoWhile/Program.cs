acc3d06e604fb18b0abc99c3b74c0c73bc19d0a4
Northman94 <Zhandarm2000@gmail.com>
2020-08-24 12:42:01 +0200

    S3E41_While_DoWhile

---
+++ CSharp_Course/S3E41_While_DoWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace S3E41_While_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+
+            while(age < 18) 
+            {
+                Console.WriteLine("What is your age?");
+                age = int.Parse(Console.ReadLine());
+            }
+
+            //- - - - - - - - - - - - - - - - - - - -
+            int count = -8;
+            do
+            {
+                Console.WriteLine(count);
+                count++;
+            }
+            while (count <= 8);
+
+            //- - - - - - - - - - - - - - - - - - - -
+            int[] numb = {-6, 4, 8, 5, 10};
+            int i = 0;
+
+            while (i < numb.Length) 
+            {
+                Console.WriteLine(numb[i] + " ");
+                i++;
+            }
+        }
+    }
+}
