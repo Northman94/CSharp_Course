16316f686c81c86bdadc6b80e5cabf81a3171357
Northman94 <zhandarm2000@gmail.com>
2020-09-06 23:00:32 +0300

    S3E45_DZ_Fibonacci
    Fibonacci sequence first try

---
+++ CSharp_Course/S3E45_DZ_Fibonacci/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace S3E45_DZ_Fibonacci
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pls Enter length of Fibonacci sequence:");
+            int Fibonacci_sequence_length = GetArrayLength();
+
+            int[] FiboArray = new int[Fibonacci_sequence_length];
+
+
+            int a = 1;
+            int b = 1;
+            int summAB = 0;
+
+
+            for (int i = 0; i < Fibonacci_sequence_length; i++)
+            {
+                if (i == 0)
+                {
+                    FiboArray[0] = a;
+                }
+                else if (i == 1)
+                {
+                    FiboArray[1] = b;
+                }
+                else
+                {
+                    summAB = a + b;
+
+                    FiboArray[i] = summAB;
+
+                    a = b;
+                    b = summAB;
+                }
+
+                Console.Write(FiboArray[i] + ", ");
+            }
+        }
+
+        static int GetArrayLength()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
