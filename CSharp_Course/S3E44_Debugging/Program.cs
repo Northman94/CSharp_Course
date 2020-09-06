f92244ed0e94337c7e71817a409f637b2f738a6b
Northman94 <zhandarm2000@gmail.com>
2020-09-06 20:46:34 +0300

    S3E44_Debugging

---
+++ CSharp_Course/S3E44_Debugging/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace S3E44_Debugging
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Square of triangle calculation");
+
+            Console.WriteLine("Enter length of AB side");
+            double AB = GetDouble();
+
+
+            Console.WriteLine("Enter length of BC side");
+            double BC = GetDouble();
+
+
+            Console.WriteLine("Enter length of CD side");
+            double AC = GetDouble();
+
+            double p = (AB + BC + AC) / 2;
+
+            double square = Math.Sqrt(p * (p - AB) * (p - BC) * (p - AC));
+            Console.WriteLine($"Square of the triangle equals {square}");
+        }
+
+
+        static double GetDouble()
+        {
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
