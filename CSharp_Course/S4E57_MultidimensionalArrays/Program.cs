d9a75bfb654bf471fcf60b82dbedfa81a66ac4ee
Northman94 <zhandarm2000@gmail.com>
2020-09-29 23:39:09 +0300

    S4E58_JaggedArray

---
+++ CSharp_Course/S4E57_MultidimensionalArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace S4E57_MultidimensionalArrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // 1 2 3
+            // 4 5 6
+
+            int[,] arr1 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+                //OR
+            int[,] arr2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            //(1) = dimention
+            for (int i = 0; i < arr2.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr2.GetLength(1); j++)
+                {
+                    Console.Write(arr2[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharp_Course/S4E58_JaggedArray/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace S4E58_JaggedArray
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Jagged Array [][] = зубчатый массив
+
+            // 1
+            // 2 3 4
+            // 5 6
+            // 7 8 9 10
+
+            int[][] jaggedArray = new int[4][]; // 4 rows Array length
+
+            jaggedArray[0] = new int[1]; //new sub-Array length (column)
+            jaggedArray[1] = new int[3]; //new sub-Array length (column)
+            jaggedArray[2] = new int[2]; //new sub-Array length (column)
+            jaggedArray[3] = new int[4]; //new sub-Array length (column)
+
+
+            Console.WriteLine("\nPlease fill the Array:");
+
+            for (int i = 0; i < jaggedArray.Length; i++) //4
+            {
+                for (int p = 0; p < jaggedArray[i].Length; p++) //Column length
+                {
+                    string st = Console.ReadLine();
+                    jaggedArray[i][p] = int.Parse(st);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Result:");
+
+            for (int i = 0; i < jaggedArray.Length; i++) //4
+            {
+                for (int p = 0; p < jaggedArray[i].Length; p++) //Column length
+                {
+                    Console.Write(jaggedArray[i][p] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
