0eab650b79463534d6be53388ff97dd48825e27d
Northman94 <zhandarm2000@gmail.com>
2020-10-11 22:35:07 +0300

    S4E59_Change_Array_Index

---
+++ CSharp_Course/Array_Index_Change/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_Index_Change
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharp_Course/S4E59_Change_Array_Index/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+ 
+
+namespace S4E59_Change_Array_Index
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Arrays with not 0 start index works slower
+
+            Array myArray = Array.CreateInstance(typeof(int), new[] {4}, new[] {1});
+            //4 elements, starting count from 1, not 0.
+
+            myArray.SetValue(2019, 1);
+            myArray.SetValue(2019, 2);
+            myArray.SetValue(2019, 3);
+            myArray.SetValue(2019, 4); //Would trigger an Exeption, if started from 0.
+
+            Console.WriteLine($"Starting index: {myArray.GetLowerBound(0)}");
+            Console.WriteLine($"Ending index: {myArray.GetUpperBound(0)} \n");
+
+            for (int i = myArray.GetLowerBound(0); i <= myArray.GetUpperBound(0); i++)
+            {
+                Console.WriteLine($"{myArray.GetValue(i)} at insex {i}");
+            }
+            Console.WriteLine();
+
+            //Same variation
+
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine($"{myArray.GetValue(i)} at insex {i}");
+            }
+        }
+    }
+}
