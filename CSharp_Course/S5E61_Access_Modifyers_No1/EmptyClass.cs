caddfbd85dbffdcac7b3e487aff190c44898d9dd
Northman94 <zhandarm2000@gmail.com>
2020-10-21 00:42:50 +0300

    S5E61_Access_Modifyers
    public
    private
    internal
    protected
    ...

---
+++ CSharp_Course/S5E61_Access_Modifyers_No1/EmptyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SecondNamespace2
+{
+    public class Character
+    {
+        private int health = 100;
+
+        public void Hit(int damage)
+        {
+            health -= damage;
+        }
+    }
+}
