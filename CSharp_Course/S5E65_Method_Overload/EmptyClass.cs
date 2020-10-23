8cb59b417a5414ea6b426559370ff10b77d0d0ff
Northman94 <zhandarm2000@gmail.com>
2020-10-24 00:45:42 +0300

    S5E65_Method_Overload
    Same name, but different parameters.

---
+++ CSharp_Course/S5E65_Method_Overload/EmptyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace S5E65_Method_Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            double square1 = calc.CalcTriangleSquare(10, 20);
+            double square2 = calc.CalcTriangleSquare(40, 20, 30);
+
+            Console.WriteLine($"Square1 = {square1}.\n Square2 = {square2}.");
+        }
+    }
+}
---
+++ CSharp_Course/S5E65_Method_Overload/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace S5E65_Method_Overload
+{
+    class Calculator
+    {
+        public double  CalcTriangleSquare(double ab, double bc, double ac)
+        {
+            double p = (ab + bc + ac) / 2;
+
+            return Math.Sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+
+        public double CalcTriangleSquare(double basis, double height)
+        {
+            return 0.5 * basis * height;
+        }
+    }
+}
