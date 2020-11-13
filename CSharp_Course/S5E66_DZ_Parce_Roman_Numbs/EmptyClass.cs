3607e00713e495ddb0faffad6c8e89ae514c01c8
Northman94 <zhandarm2000@gmail.com>
2020-11-14 00:15:17 +0200

    S5E66_DZ_Parce_Roman_Numbs
    Great Solution. Though have issues to include

---
+++ CSharp_Course/S5E66_DZ_Parce_Roman_Numbs/EmptyClass.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace S5E66_DZ_Parce_Roman_Numbs
+{
+    public class Roman_Numeral
+    {
+        // III = 1+1+1
+        //  IV = 5-1
+        // XIV = 10-1+5
+
+        private static Dictionary <char, int> maping = new Dictionary <char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000}
+        };
+
+        public static int Parcing(string roman)
+        {
+            int result = 0;
+
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if(i+1 < roman.Length && IsSubstractive(roman[i], roman[i+1]))
+                {
+                    result -= maping[roman[i]];
+                }
+                else
+                {
+                    result += maping[roman[i]];
+                }
+            }
+            return result;
+        }
+
+
+        private static bool IsSubstractive(char c1, char c2)
+        {
+            return maping[c1] < maping[c2];
+        }
+    }
+}
---
+++ CSharp_Course/S5E66_DZ_Parce_Roman_Numbs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S5E66_DZ_Parce_Roman_Numbs
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Roman_Numeral.Parcing("XVI"));
+        }
+    }
+}
