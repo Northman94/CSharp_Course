f9fe8c51ce4edeea3c7ca5f18415b8cbf383eb42
Northman94 <zhandarm2000@gmail.com>
2020-11-19 22:02:07 +0200

    S5E70_Params

---
+++ CSharp_Course/S5E70_Params/EmptyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OverridenMethods
+{
+    public class Triangle_Overload
+    {
+        public double Average(int[] numbs)
+        {
+            int sum1 = 0;
+
+            foreach (int n in numbs)
+            {
+                sum1 += n;
+            }
+            return (double)sum1 / numbs.Length; 
+        }
+
+
+        // Implicit Arr creation.
+        //In case of giving multiple arguments
+        //the one with params should be last
+        public double Average2(params int[] numbs)
+        {
+            int sum2 = 0;
+
+            foreach (int n in numbs)
+            {
+                sum2 += n;
+            }
+            return (double)sum2 / numbs.Length;
+        }
+    }
+}
---
+++ CSharp_Course/S5E70_Params/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OverridenMethods;
+
+namespace S5E70_Params
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Triangle_Overload tr_sq = new Triangle_Overload();
+
+
+            double aver1 = tr_sq.Average(new int[] {1, 2, 3, 4});
+
+            double aver2 = tr_sq.Average2(5, 4, 3, 2, 1);
+
+            Console.WriteLine($" Aver1 = {aver1}; \n Aver2 = {aver2};");
+        }
+    }
+}
