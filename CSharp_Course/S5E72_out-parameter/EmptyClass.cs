ff53d96ac7e582e78c2cf4ba8b2ec76ee344e4b1
Northman94 <zhandarm2000@gmail.com>
2020-12-06 23:08:33 +0200

    S5E72_out parameter

---
+++ CSharp_Course/S5E72_out-parameter/EmptyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace S5E72_outparameter
+{
+    public class Out_calc
+    {
+        public bool TryDivide(double divisible, double divisor, out double res)
+        {
+            res = 0; //always initialize OUT
+
+            if (divisor == 0)
+            {
+                return false;
+            }
+            else
+            {
+                res = divisible / divisor;
+                return true;
+            }
+        }
+    }
+}
---
+++ CSharp_Course/S5E72_out-parameter/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace S5E72_outparameter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Out_calc calc = new Out_calc();
+
+            if (calc.TryDivide(divisible: 10, divisor: 2, out double res))
+            {
+                Console.WriteLine(res);
+            }
+            else
+            {
+                Console.WriteLine("Division Failed");
+            }
+
+            /*
+             
+            Console.WriteLine("Pls, enter a number");
+            string line = Console.ReadLine();
+
+            //Before C# 6:
+            int numb;
+            bool wasParsed = int.TryParse(line, out numb);
+            //Try-methods use out modificator
+
+            if (wasParsed)
+            {
+                Console.WriteLine(numb);
+            }
+            else
+            {
+                Console.WriteLine("Failed to Parce.");
+            }
+
+            Console.WriteLine(wasParsed);
+
+            */
+
+            Console.WriteLine("Pls, enter a number");
+            string line = Console.ReadLine();
+
+           
+            bool wasParsed = int.TryParse(line, out int result); //or var
+            //Try-methods use out modificator
+
+            if (wasParsed)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Failed to Parce.");
+            }
+
+            Console.WriteLine(wasParsed);
+
+            // // // // // // // // // // // // // // //
+
+
+            
+            
+        }
+    }
+}
