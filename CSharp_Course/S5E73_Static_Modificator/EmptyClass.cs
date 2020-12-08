b680f8c43bc21c9a9eff2ff3c99012391e6ae14d
Northman94 <zhandarm2000@gmail.com>
2020-12-08 23:10:28 +0200

    S5E73_Static_Modificator

---
+++ CSharp_Course/S5E73_Static_Modificator/EmptyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace S5E73_Static_Modificator
+{
+    public class Calculator
+    {
+        //static - method
+        public static double Average(double x, double y, double z)
+        {
+            return (x + y + z) / 3;
+        }
+
+        // this Class can be static to prevent
+        // Calculator Class instance creation
+        // in another Class.
+    }
+}
---
+++ CSharp_Course/S5E73_Static_Modificator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace S5E73_Static_Modificator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Due to method being static, no need to create
+            // Class instance (экз. класса), to adress to the method
+            
+            double result = Calculator.Average(4, 5, 6);
+
+            Console.WriteLine(result);
+        }
+    }
+}
