202ca7d5f0ee0ab41dbc33d1e69a9b2580baf708
Northman94 <zhandarm2000@gmail.com>
2020-12-17 23:30:37 +0200

    S5E74_Optional_Parameters

---
+++ CSharp_Course/S5E74_Optional_Parameters/EmptyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace S5E74_Optional_Parameters
+{
+    public class E73_Class
+    {
+        public static double CalcTriangleSquare(double ab, double ac, int alpha, bool isInRadians = false)
+        {
+            //Check on passing Radians type
+            if (isInRadians)
+            {
+                return 0.5 * ab * ac * Math.Sin(alpha);
+            }
+            else
+            {
+                double inRadians = alpha * Math.PI / 180;
+
+                return 0.5 * ab * ac * Math.Sin(inRadians);
+            }   
+        }
+    }
+}
---
+++ CSharp_Course/S5E74_Optional_Parameters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S5E74_Optional_Parameters
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(E73_Class.CalcTriangleSquare(4, 5, 6));
+        }
+    }
+}
