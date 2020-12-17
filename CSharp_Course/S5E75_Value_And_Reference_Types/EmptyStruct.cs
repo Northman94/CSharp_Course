c4a4d4691b91e54349d6a6413f0e61a0a653df97
Northman94 <zhandarm2000@gmail.com>
2020-12-18 00:46:22 +0200

    namespace S5E75_Value_And_Reference_Types

---
+++ CSharp_Course/S5E75_Value_And_Reference_Types/EmptyStruct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace S5E75_Value_And_Reference_Types
+{
+    //Structure => Value Type
+    public struct PointVal
+    {
+        public int X;
+
+        public int Y;
+
+
+        public void LogValues()
+        {
+            Console.WriteLine($" X = {X} & Y = {Y};");
+        }
+    }
+
+    //Class => Reference Type
+    public class PointRef
+    {
+        public int X;
+
+        public int Y;
+
+
+        public void LogValues()
+        {
+            Console.WriteLine($" X = {X} & Y = {Y};");
+        }
+    }
+}
---
+++ CSharp_Course/S5E75_Value_And_Reference_Types/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace S5E75_Value_And_Reference_Types
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Экземпляр структуры:
+            PointVal a1; //or PointVal a1 = new PointVal();
+
+            a1.X = 3;
+            a1.Y = 5;
+
+            PointVal b2 = a1;
+
+            b2.X = 7;
+            b2.Y = 10;
+
+
+            a1.LogValues(); //X = 3 & Y = 5;
+            b2.LogValues(); //X = 7 & Y = 10;
+
+            Console.WriteLine("\n\n");
+
+            //Ref types only fuly declared:
+            PointRef c3 = new PointRef();
+
+            c3.X = 3;
+            c3.Y = 5;
+
+            PointRef d4 = c3;
+
+            d4.X = 7;
+            d4.Y = 10;
+
+
+            c3.LogValues(); // X = 7 & Y = 10;
+            d4.LogValues(); // X = 7 & Y = 10;
+        }
+    }
+}
