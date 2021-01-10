3844e493b64744156d7d6a5394ad22a1149d8860
Northman94 <zhandarm2000@gmail.com>
2021-01-11 00:13:09 +0200

    S5E80_BoxingAndUnboxing

---
+++ CSharp_Course/S5E79_NullReferenceExeption/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace S5E79_NullReferenceExeption
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            PointRef c; //OR  = null;
+
+            Console.WriteLine(c.X); // NullReferenceExeprion
+            //-- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+            PointVal? pv = null;
+
+            if (pv.HasValue)
+            {
+                PointVal pv2 = pv.Value;
+
+                Console.WriteLine(pv.Value.X);
+                Console.WriteLine(pv2.X); //Same
+            }
+
+            //OR THIS VARIATION instead of if block:
+            PointVal pv3 = pv.GetValueOrDefault();
+        }
+
+        static struct PointVal
+        {
+            public int X;
+            public int Y;
+
+            public void LogValues()
+            {
+                Console.WriteLine($"X = {X}; Y = {Y};");
+            }
+        }
+    }
+}
---
+++ CSharp_Course/S5E80_BoxingAndUnboxing/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace S5E80_BoxingAndUnboxing
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = 1;
+
+            object obj1 = x; //boxing process
+            //object is a reference type
+
+            int y = (int)obj1; //unboxing
+            // -- -- -- -- -- -- -- -- --
+            
+
+            //Should get InvalidCastExeption:
+            //Can't cast System.Double to System.Int32
+            double pi = 3.14;
+            object obj2 = pi;
+
+            int number = (int)pi;
+            Console.WriteLine(number);
+
+            //Can use:
+            number = (int)(double)pi;
+            Console.WriteLine(number);
+
+            Do(obj2);
+        }
+
+        static void Do(object obj2)
+        {
+            bool isPointRef = obj2 is PointRef;
+
+            if (isPointRef)
+            {
+                PointRef pr = (PointRef) obj2;
+                //obj.
+                Console.WriteLine(pr.X);
+            }
+
+            //OR
+
+            PointRef pr1 = obj2 as PointRef;
+            if (pr1 != null)
+            {
+                Console.WriteLine(pr1.X);
+            }
+        }
+
+        private class PointRef
+        {
+            public int X;
+            public int Y;
+
+            public void LogValues()
+            {
+                Console.WriteLine($"X = {X}; Y = {Y};");
+            }
+        }
+    }
+}
