265005310319ef00b9da803a86ab0bc556aead95
Northman94 <zhandarm2000@gmail.com>
2021-01-19 22:14:11 +0200

    S5E83_Inheritance

---
+++ CSharp_Course/S5E83_Inheritance/EmptyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace S5E83_Inheritance
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Model_X_Terminal x_Terminal = new Model_X_Terminal("123");
+            x_Terminal.Connect();
+
+            Console.WriteLine();
+
+            Model_S_Terminal s_Terminal = new Model_S_Terminal("789");
+            s_Terminal.Connect();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp_Course/S5E83_Inheritance/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace S5E83_Inheritance
+{
+    //Example of three Bank terminals communication with server
+
+    // "virtual" is used so some heirs could use basic class method & some could reuse it
+
+    public class BankTerminal
+    {
+        protected string id;
+
+        //rewritten Constructor for all heirs:
+        public BankTerminal(string id)
+        {
+            this.id = id;
+        }
+
+        public virtual void Connect()
+        {
+            Console.WriteLine($"Basic Connection Protocol {id}.");
+
+        }
+
+    }
+    //-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+    public class Model_X_Terminal : BankTerminal
+    {
+        /*
+        //Constructor:
+        public Model_X_Terminal(string id)
+        {
+            base.id = id;
+        }
+        //Can be rewritten for all heirs in base class
+        */
+        // And give all work to base constructor:
+        public Model_X_Terminal(string id) : base(id)
+        {
+
+        }
+
+        public override void Connect()
+        {
+            //to use basic variation of this method "base" key word is used
+
+            base.Connect();
+            Console.WriteLine("Aditional actions for Model_X");
+        }
+    }
+    //-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+    public class Model_S_Terminal : BankTerminal
+    {
+        public Model_S_Terminal(string id) : base(id)
+        {
+            base.id = id;
+        }
+
+        public override void Connect()
+        { 
+           Console.WriteLine($"Aditional actions for Model_S {id}");
+        }
+    }
+}
+    
+
