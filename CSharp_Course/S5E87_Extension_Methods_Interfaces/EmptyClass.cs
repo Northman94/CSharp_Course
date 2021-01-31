40f4f525b1c469bac2fc5043a11f4b201f484e21
Northman94 <zhandarm2000@gmail.com>
2021-02-01 01:04:38 +0200

    S5E87_Extension_Methods_Interfaces

---
+++ CSharp_Course/S5E87_Extension_Methods_Interfaces/EmptyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S5E87_Extension_Methods_Interfaces
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<object> list = new List<object>() { 1, 2, 3};
+
+            IBaseCollection baseCollection = new BaseList(4);
+
+
+            baseCollection.AddRange(list);
+
+            //Boxing because 8 is a value type (obj)
+            baseCollection.Add(8);
+
+        }
+    }
+}
---
+++ CSharp_Course/S5E87_Extension_Methods_Interfaces/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S5E87_Extension_Methods_Interfaces
+{
+    public interface IBaseCollection
+    {
+        //Will have basic functionality.
+        void Add(object obj);
+
+        void Remove(object obj);
+    }
+
+    //Extension method:
+    public static class BaseCollectionExtension
+    {
+        public static void AddRange(this IBaseCollection IColl, IEnumerable <object> objects)
+        {
+            foreach (var item in objects)
+            {
+                IColl.Add(item);
+            }
+        }
+    }
+
+    public class BaseList : IBaseCollection
+    {
+        private int count = 0;
+        private object[] items;
+
+        public BaseList(int initialCapacity)
+        {
+            items = new object[initialCapacity];
+        }
+
+
+        public void Add(object obj)
+        {
+            items[count] = obj;
+            count++;
+        }
+
+        public void Remove(object obj)
+        {
+            items[count] = null;
+            count--;
+        }
+    }
+}
+
