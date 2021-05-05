9c789cda3d8b40f1803326858219252cf4ad0174
Northman94 <Zhandarm2000@gmail.com>
2021-05-05 22:25:05 +0300

    .csv added + class variables

--- CSharp_Course/S9E112_Parse_to_CSV_File/Program.cs
+++ CSharp_Course/S9E112_Parse_to_CSV_File/Program.cs
@@ -2,6 +2,21 @@
 
 namespace S9E112_Parse_CSV_File
 {
+    public class BaseballPlayers
+    {
+        public string Name { get; set;}
+
+        public string Team { get; set;}
+
+        public string Position { get; set; }
+
+        public int Height_inches { get; set; }
+
+        public int Weight_LBS { get; set; }
+
+        public float Age { get; set; }
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
